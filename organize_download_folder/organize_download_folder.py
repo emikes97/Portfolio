2a8650d3a094a32d0887d38628de34019044d42b
emikes97 <mikesemmanouil@outlook.com.gr>
2025-05-30 02:45:26 +0300

    Added logging also, which will keep every files that has been moved, there is some issues that we will fix though

--- organize_download_folder/organize_download_folder.py
+++ organize_download_folder/organize_download_folder.py
@@ -1,9 +1,10 @@
+import logging
 import os
 import shutil
 from pathlib import Path
+from datetime import datetime
 
-download_path = Path.home() / "Downloads"
-
+# All file types that the script will scan and move to respective folders.
 file_types = {
     'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp'],
     'Documents': ['.pdf', '.docx', '.txt', '.xlsx', '.pptx'],
@@ -12,8 +13,26 @@ file_types = {
     'Archives': ['.zip', '.rar', '.7z', '.tar'],
     'Scripts': ['.py', '.js', '.sh', '.bat'],
     'Installers': ['.exe', '.msi', '.dmg'],
-    'Others': []  # fallback
+    'Others': []
 }
+
+download_path = Path.home() / "Downloads" # Initiate the download path
+
+# Add logging for each moved file.
+log_file = Path.home() / "Downloads" / "organizer_log.txt"
+
+if not log_file.exists():
+    """If file don't exist, create it and add a header also"""
+    log_file.touch() # Create new file, if it doesn't exist.
+    with open(log_file, 'w') as file:
+        file.write("Download Organizer Log\n")
+        file.write("=" * 40 + "\n")
+
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 for file in download_path.iterdir():
     if file.is_file():
@@ -23,11 +42,13 @@ for file in download_path.iterdir():
                 target_folder = download_path / category
                 target_folder.mkdir(exist_ok=True)
                 shutil.move(str(file), str(target_folder / file.name))
+                logging.info(f"Moved '{file.name}' -> '{category}/'")
                 moved = True
                 break
         if not moved:
             other_folder = download_path / "Others"
             other_folder.mkdir(exist_ok=True)
             shutil.move(str(file), str(other_folder / file.name))
+            logging.info(f"Moved '{file.name}' -> 'Others/")
 
 print("Download folder organized successfully!")
